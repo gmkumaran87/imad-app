6008917fefad22f54615d98c601d036fe829aa5d
gmkumaran87 <gmkumaran87@gmail.com>
2017-08-31 23:29:18 +0530

    Function created for Article One.

--- server.js
+++ server.js
@@ -5,12 +5,68 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+	title: "Article One |Muthukumaran G",
+	heading: "Article-One",
+	date: "31st Aug 2017",
+	comment: `<p> This is my first Article under this Web page </p>
+	  <p> This is my first Article under this Web page </p>
+	  <p> This is my first Article under this Web page </p>
+	   `
+};
+
+function createTemplate(data){
+	var title = data.title:
+	var head = data.heading;
+	var date = data.date;
+	var comment = data.comment;
+	
+	var htmlTemplate = `
+	<html>
+	<head>
+	<title>
+	${title}
+          </title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link href="/ui/style.css" rel="stylesheet" />
+      </head> 
+      <body>
+          <div class="container">
+              <div>
+                  <a href="/">Home</a>
+              </div>
+              <hr/>
+              <h3>
+                  ${heading}
+              </h3>
+              <div>
+                  ${date.toDateString()}
+              </div>
+              <div>
+                ${content}
+              </div>
+              <hr/>
+              <h4>Comments</h4>
+              <div id="comment_form">
+              </div>
+              <div id="comments">
+                <center>Loading comments...</center>
+              </div>
+          </div>
+          <script type="text/javascript" src="/ui/article.js"></script>
+      </body>
+    </html>
+    `;
+	return htmlTemplate;
+};
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(re,res){
-  res.sendFile(path.join(__dirname,'ui', 'article-one.html'));
+ // res.sendFile(path.join(__dirname,'ui', 'article-one.html'));
+  res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two',function(req,res){
