fda2963e898d84c26d831728e2f8d7d10acafc9d
gmkumaran87 <gmkumaran87@gmail.com>
2017-09-06 22:59:27 +0530

    fdff

--- server.js
+++ server.js
@@ -91,8 +91,8 @@ app.get('/', function (req, res) {
 
 app.get('/articles/:articleName',function(req,res){
 
- var articleName = ;
- pool.query("SELECT * FROM ARTICLE WHERE TITLE = '"+ req.params.articleName+"'", function(err,result){
+ //var articleName = ;
+ pool.query("SELECT * FROM ARTICLE WHERE TITLE = '"+ req.params.articleName +"'", function(err,result){
 	 if(err) {
 			res.status(500).send(err.toString());
 		}else{
