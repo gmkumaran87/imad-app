b1b9179effda8e3b9fbd5f2ddd00a56c4a51ebbb
gmkumaran87 <gmkumaran87@gmail.com>
2017-09-21 23:25:13 +0530

    kk

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,11 @@
 console.log('Loaded!');
 
-var img = document.getElementById('madi');
+
 var marginLeft = 0;
 /*function moveRight(){
 	marginLeft = marginLeft + 10;
 	img.style.marginLeft = marginLeft + 'px';*/
+var img = document.getElementById('madi');
 img.onclick = function(){
 	img.style.marginLeft = '100px';
 	//var  interval = setInterval(moveRight,50)
