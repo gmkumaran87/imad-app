3becae36f67e5b9e0133f826959d24193da84fb6
Muthukumaran <30677481+gmkumaran87@users.noreply.github.com>
2017-09-21 23:29:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,3 @@
 console.log('Loaded!');
 
 
-var marginLeft = 0;
-/*function moveRight(){
-	marginLeft = marginLeft + 10;
-	img.style.marginLeft = marginLeft + 'px';*/
-var img = document.getElementById('madi');
-img.onclick = function(){
-	img.style.marginLeft = '100px';
-	//var  interval = setInterval(moveRight,50)
-};
