233f4f2710f2d3fd48be9855ddf31da37a0ffbc2
gmkumaran87 <gmkumaran87@gmail.com>
2017-09-21 23:42:13 +0530

    Margin left

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,12 @@ console.log('Loaded!');
 
 
 var img = document.getElementById('madi');
+var marginLefgt = 0;
+function moveRight(){
+	marginLeft = marginLeft + 10;
+	img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function(){
-    img.style.marginLeft = '100px';
+	var interval = setInterval(moverRight,100);
+    
 };
