05dddb0bb68848c4bb48f9b9c8542ffbde939a7d
gmkumaran87 <gmkumaran87@gmail.com>
2017-09-21 23:21:36 +0530

    madi..

--- ui/main.js
+++ ui/main.js
@@ -2,10 +2,10 @@ console.log('Loaded!');
 
 var img = document.getElementById('madi');
 var marginLeft = 0;
-function moveRight(){
+/*function moveRight(){
 	marginLeft = marginLeft + 10;
-	img.style.marginLeft = marginLeft + 'px';
+	img.style.marginLeft = marginLeft + 'px';*/
 img.onclick = function(){
-	
-	var  interval = setInterval(moveRight,50)
+	img.style.marginLeft = '100px';
+	//var  interval = setInterval(moveRight,50)
 };
