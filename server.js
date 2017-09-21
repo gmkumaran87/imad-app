531750bb4118f78fa84827b43ab22b5cd6f09f36
gmkumaran87 <gmkumaran87@gmail.com>
2017-09-22 00:03:34 +0530

    /counter set

--- server.js
+++ server.js
@@ -89,6 +89,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0
+app.get('/counter',function() {
+	counter = counter+1;
+	res.send(counter.toString());
+});
 app.get('/articles/:articleName',function(req,res){
 
  //var articleName = ;
