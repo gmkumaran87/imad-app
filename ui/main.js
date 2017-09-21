221f71b9515014fb2a93729c8c2bab173780a84d
Muthukumaran <30677481+gmkumaran87@users.noreply.github.com>
2017-09-21 23:36:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 
 
-var img = document.getElementbyId('madi');
+var img = document.getElementById('madi');
 img.onclick = function(){
     img.style.marginLeft = '100px';
 };
